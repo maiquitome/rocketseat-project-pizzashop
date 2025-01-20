10da814c36c4be2a92cc36f396c90926ee68a8d7
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2025-01-20 12:00:48 -0300

    build: :heavy_plus_sign: add button with `$ npx shadcn@latest add button`

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 export function App() {
   return (
-    <>
+    <div className="flex justify-center items-center h-screen">
       <h1>Hello World!</h1>
-    </>
+      <Button>Enviar</Button>
+    </div>
   );
 }
