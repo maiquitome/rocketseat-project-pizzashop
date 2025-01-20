33f420bdbd8225d313d311d0edc1dd958aae16ca
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2025-01-20 13:29:48 -0300

    chore: :wrench: create `.prettierrc` file with `prettier-plugin-tailwindcss` config

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Button } from "./components/ui/button";
 
 export function App() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex h-screen items-center justify-center">
       <h1>Hello World!</h1>
       <Button>Enviar</Button>
     </div>
