ab9423650f00e3395e0e0c0585a78bc0ac820a65
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2025-02-04 11:01:04 -0300

    feat: :sparkles: add `toast` to `handleSignIn`

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
@@ -9,17 +10,33 @@ import { Label } from "@/components/ui/label";
 const signInForm = z.object({
   email: z.string().email(),
 });
+
 type SignInForm = z.infer<typeof signInForm>;
+
 export function SignIn() {
   const {
     register,
     handleSubmit,
     formState: { isSubmitting },
   } = useForm<SignInForm>();
+
   async function handleSignIn(data: SignInForm) {
-    console.log(data);
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+
+      toast.success("Enviamos um link de autenticação para seu e-mail.", {
+        action: {
+          label: "Reenviar",
+          onClick: () => {
+            handleSignIn(data);
+          },
+        },
+      });
+    } catch {
+      toast.error("Credenciais inválidas.");
+    }
   }
+
   return (
     <>
       <Helmet title="Login" />
